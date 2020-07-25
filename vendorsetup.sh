4c3ac64f6bdcb3b83f625ee42784279c2cb77679
Unknown <electrikjesus@gmail.com>
2020-07-25 11:56:27 -0400

    Utdate for use as a vendor method

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,9 +1,8 @@
+rompath=$(pwd)
+vendor_path="surface-drivers"
+
+
 function apply-ipts-patches()
 {
-	if  [ $1 == "4.19" ];then
-		$(foreach f, $(wildcard external/surface-drivers/ipts/patches/4.19/*.patch), $(eval patch -p1 -d $(f)))
-	else
-		echo "We are sorry, but we currently don't support kernel $1"
-	fi
-
+	vendor/${vendor_path}/autopatch.sh $1
 }
